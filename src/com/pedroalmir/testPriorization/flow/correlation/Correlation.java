86ea7eadabbcbef9806a183f327f8a11bb2733b9
MatheusCampanha <matheusmmcs@gmail.com>
2013-10-27 23:45:04 -0300

    Insercao de distribuicao normal

--- src/com/pedroalmir/testPriorization/flow/correlation/Correlation.java
+++ src/com/pedroalmir/testPriorization/flow/correlation/Correlation.java
@@ -9,26 +9,59 @@ import java.util.Random;
 import au.com.bytecode.opencsv.CSVWriter;
 
 public class Correlation {
-	private final static String BASE_FOLDER = "C:/Users/Pedro Almir/Dropbox/UFPI/Mestrado/Prioriza√ß√£o de Casos de Teste/Arquivos Base/Aleatorios/DadosAleatorios/";
+	private final static String BASE_FOLDER = "C:/Eclipse/junojee/mestrado/regressionTestingPriorization/experimentos/";
     
     public static void main(String[] args) throws IOException {
             int contClass;
             
             //1000-20
             contClass = 1000;
-            generetaFiles(contClass, contClass/50);
+           // generetaFiles(contClass, contClass/50);
             
             //1000-100
             contClass = 1000;
-            generetaFiles(contClass, contClass/10);
+            //generetaFiles(contClass, contClass/10);
             
             //5000-500
             contClass = 5000;
-            generetaFiles(contClass, contClass/10);
+           // generetaFiles(contClass, contClass/10);
             
             //10000-200
             contClass = 1000;
-            generetaFiles(contClass, contClass/50);
+           // generetaFiles(contClass, contClass/50);
+            
+            //to see more about gaussian number generated, open this link: http://www.javamex.com/tutorials/random_numbers/gaussian_distribution_2.shtml
+           // Correlation.testGaussianNumberGenerate(0,15,15,30,45);
+            //Correlation.testGaussianNumberGenerate(0,100,90,160,210);//115,125,145,170
+            
+            //CORRELA«√O
+            int cont0 = 0, cont1 = 0, cont3 = 0, cont9 = 0;
+            for(int i = 0; i < 100; i++){
+            	double generateGaussianNumber = generateGaussianNumber(0, 100);
+            	if(generateGaussianNumber >= 210){
+            		cont9++;
+            	}else if(generateGaussianNumber >= 160){
+            		cont3++;
+            	}else if(generateGaussianNumber >= 90){
+            		cont1++;
+            	}else{
+            		cont0++;
+            	}
+            }
+            System.out.println(cont0+" - "+cont1+" - "+cont3+" - "+cont9);
+            
+            //TESTE
+            int countContainsCoverage = 0;
+            for(int i = 0; i < 5000; i++){
+            	double generateGaussianNumber = generateGaussianNumber(0, 30);
+            	if(generateGaussianNumber >= 60){
+            		countContainsCoverage++;
+            		generateGaussianNumber = generateGaussianNumber < 100 ? generateGaussianNumber : 100; 
+            		System.out.println(generateGaussianNumber);
+            	}
+            }
+            System.out.println("--"+countContainsCoverage+"--");
+            
     }
     
     public static void generetaFiles(int contClass, int reqCount) throws IOException{
@@ -158,5 +191,41 @@ public class Correlation {
             
             writer.writeAll(data);
             writer.close();
+    }
+    
+    public static void testGaussianNumberGenerate(int centerValue, int standardDeviation, int x1, int x2, int x3){
+        int inside15 = 0, inside30 = 0, inside45 = 0, count = 1000;
+        double maxval = 0, val;
+        for(int i = 0; i < count; i++){
+        	val = generateGaussianNumber(centerValue, standardDeviation);
+        	if(val <= x1){
+        		inside15++;
+        	}
+        	if(val <= x2){
+        		inside30++;
+        	}
+        	if(val <= x3){
+        		inside45++;
+        	}
+        	if(maxval<val){
+        		maxval = val;
+        	}
+        	System.out.println("val: "+val);
+        }
+        System.out.println("maxval: "+maxval+" in: "+count);
+        System.out.println(x1+": "+inside15+" - "+new Double(inside15)/(count/100));
+        System.out.println(x2+": "+inside30+" - "+new Double(inside30)/(count/100));
+        System.out.println(x3+": "+inside45+" - "+new Double(inside45)/(count/100));
+    }
+    
+    public static double generateGaussianNumber(int centerValue, int standardDeviation){
+    	Random random = new Random();
+    	double val, dif;
+    	val = random.nextGaussian() * standardDeviation + centerValue;
+    	dif = (centerValue-val);
+    	//generate values larger than centerValue
+    	dif = dif < 0 ? -dif : dif;
+    	val = centerValue + dif;
+    	return val;
     }
 }
