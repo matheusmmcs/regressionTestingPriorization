5496ae82042116daedc6dd33b40cc42f91811212
cleitonmoura18 <cleitonmoura18@gmail.com>
2013-10-31 19:22:46 -0300

    Matheus - Alteração na geração dos dados de requisitos

--- src/com/pedroalmir/testPriorization/util/enums/EnumJhmType.java
+++ src/com/pedroalmir/testPriorization/util/enums/EnumJhmType.java
@@ -1,15 +1,21 @@
 package com.pedroalmir.testPriorization.util.enums;
 
 public enum EnumJhmType {
-	CLASS_MAPPING("class-mapping"), FLOW("flow"), REPORT("report");
+	CLASS_MAPPING("<class-mapping","class-mapping"), FLOW("<flow","flow"), REPORT("<report","report");
 
+	private String xml;
 	private String description;
 
-	EnumJhmType(String description) {
+	EnumJhmType(String xml, String description) {
+		this.xml = xml;
 		this.description = description;
 	}
 
 	public String getDescription() {
 		return this.description;
+	}
+	
+	public String getXml() {
+		return this.xml;
 	}
 }
